6ceee6640bcdcf2fd12b36270dcfa17ee6513489
Jesper Hallborg <jesper.hallborg@ericsson.com>
2019-05-28 15:22:04 +0200

    Part 1; Chapter 3: Equality for all

--- src/main/scala/part_1/Dollar.scala
+++ src/main/scala/part_1/Dollar.scala
@@ -4,4 +4,6 @@ class Dollar(val amount: Int) {
 	def times(multiplier: Int):Dollar = {
 		new Dollar(amount * multiplier)
 	}
+
+	override def equals(obj: Any): Boolean = obj.asInstanceOf[Dollar].amount == this.amount
 }
--- src/test/scala/part_1/DollarTest.scala
+++ src/test/scala/part_1/DollarTest.scala
@@ -14,5 +14,18 @@ final class DollarTest extends Specification {
 			ten.amount shouldEqual 10
 			fifteen.amount shouldEqual 15
 		}
+
+		"be equal to another instance of dollar" in {
+			val five1 = new Dollar(5)
+			val five2 = new Dollar(5)
+
+			five1 shouldEqual five2
+		}
+		"be unequal to another instance of dollar" in {
+			val five = new Dollar(5)
+			val six = new Dollar(6)
+
+			five shouldNotEqual six
+		}
 	}
 }
